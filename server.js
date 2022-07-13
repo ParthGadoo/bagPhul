7c5409125cfa889ce2a96b68194a76935d3af96f
Parth Gadoo <p.gadoo@iitg.ac.in>
2022-07-13 11:08:23 +0530

    login&reg_page_done

--- server.js
+++ server.js
@@ -6,13 +6,26 @@ const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.render("home.ejs");
-});
 
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 app.listen(PORT, () => {
   console.log(`Serving on port ${PORT}`);
