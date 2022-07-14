e97bf9172f19d5dc6e7f88e422f4b48872209961
Parth Gadoo <p.gadoo@iitg.ac.in>
2022-07-14 12:58:13 +0530

    cart_logic _added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-console.log("Hello from app.js");
+import axios from "axios";
+
+let addToCart = document.querySelectorAll(".add-to-cart");
+let cartCounter = document.querySelector("#cartCounter");
+
+const updateCart = (product) => {
+  axios.post("/update-cart", product).then((res) => {
+    console.log(res);
+    cartCounter.innerText = res.data.totalQty;
+  });
+};
+
+addToCart.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let product = JSON.parse(btn.dataset.product);
+    updateCart(product);
+  });
+});
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
@@ -5,6 +7,9 @@ const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo");
 
 const url = "mongodb://localhost:27017/bagPhul-test";
 
@@ -18,7 +23,27 @@ mongoose
     console.log(err);
   });
 
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoDbStore.create({
+      mongoUrl: url,
+    }),
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
+
+app.use(flash());
+
 app.use(express.static("public"));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
