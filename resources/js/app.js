4e7177c96ea1787fc0e1080340c020d2d3a748d6
Parth Gadoo <p.gadoo@iitg.ac.in>
2022-07-20 18:10:28 +0530

    noty_added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,27 @@
 import axios from "axios";
+import Noty from "noty";
 
 let addToCart = document.querySelectorAll(".add-to-cart");
 let cartCounter = document.querySelector("#cartCounter");
 
 const updateCart = (product) => {
-  axios.post("/update-cart", product).then((res) => {
-    cartCounter.innerText = res.data.totalQty;
-  });
+  axios
+    .post("/update-cart", product)
+    .then((res) => {
+      cartCounter.innerText = res.data.totalQty;
+      new Noty({
+        text: "Item added to cart",
+        timeout: 1000,
+        type: "success",
+      }).show();
+    })
+    .catch((err) => {
+      new Noty({
+        text: "Something went wrong",
+        timeout: 1000,
+        type: "error",
+      }).show();
+    });
 };
 
 addToCart.forEach((btn) => {
